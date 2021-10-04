644cf42d8e74bd68e2a4f159089032cb18d19af7
Olavo-DIO <olavo.melo.br.DIO@gmail.com>
2021-10-04 16:28:22 -0300

    App Agenda de eventos DIO - Curso Python Django Commit 002 Após os templates e redirecionamento do '' para /agenda/

--- diodjango/urls.py
+++ diodjango/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 from core import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('agenda/', views.Lista_eventos),
-    path('', views.Lista_eventos),
+    #path('', views.Lista_eventos),
+    path('', RedirectView.as_view(url='/agenda'))
 ]
