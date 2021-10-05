49a1d8061b202feb109f14ead8f8629fa9603fcd
Olavo-DIO <olavo.melo.br.DIO@gmail.com>
2021-10-05 08:24:50 -0300

    App Agenda de eventos DIO - Curso Python Django Commit 005 Excluir e editar eventos

--- core/admin.py
+++ core/admin.py
@@ -4,7 +4,7 @@ from core.models import Evento
 
 
 class EventoAdmin(admin.ModelAdmin):
-    list_display = ('titulo','data_evento','data_criacao')      # altera o header da table no admin
+    list_display = ('id','titulo','data_evento','data_criacao')      # altera o header da table no admin
     list_filter = ('usuario','data_evento',)                    # cria filtros no admin
 
 admin.site.register(Evento, EventoAdmin)
